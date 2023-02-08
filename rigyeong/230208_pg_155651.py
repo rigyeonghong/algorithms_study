5822b3a44be0d6bc38ed3f359b44bd35743f38d6
Rigyeong Hong <77164776+rigyeonghong@users.noreply.github.com>
2023-02-08 20:47:08 +0900

    Update 230208_pg_155651.py
    mine

--- rigyeong/230208_pg_155651.py
+++ rigyeong/230208_pg_155651.py
@@ -1,5 +1,3 @@
-from heapq import heappop, heappush
-
 def solution(book_time):
     cnt = 1
     book_times = []
@@ -8,18 +6,17 @@ def solution(book_time):
         end = int(time[1][0:2]) * 60 + int(time[1][3:])
         book_times.append([start, end])
     book_times.sort()
-    
-    heap =[]
-    for start, end in book_times:
-        if heap == []:
-            heappush(heap, end+10)
-            continue
 
-        if heap[0] <= start:
-            heappop(heap)
+    rooms =[]
+    for s, e in book_times:
+        if rooms == []:
+            rooms.append(e+10)
+            continue
+        rooms.sort()
+        if rooms[0] <= s:
+            rooms[0] = e+10
         else:
-            cnt +=1
-        heappush(heap, end+10)
-    return cnt
+            rooms.append(e+10)
+    return len(rooms)
 
 print(solution([["15:00", "17:00"], ["16:40", "18:20"], ["14:20", "15:20"], ["14:10", "19:20"], ["18:20", "21:20"]]))
