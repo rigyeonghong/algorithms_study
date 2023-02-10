0dcfafaff15c54d82c2b029a9f07f1ff87586761
rigyeonghong <77164776+rigyeonghong@users.noreply.github.com>
2023-02-11 08:46:17 +0900

    binary search

---
+++ rigyeong/230212_pg_43238.py
@@ -0,0 +1,18 @@
+def solution(n, times):
+    minutes = 0
+    left, right = 1, max(times) * n
+    while left <= right:
+        mid = (left+right) // 2
+        people = 0
+        for time in times:
+            people += mid // time
+            if people >= n:
+                break
+                
+        if people >= n:
+            minutes = mid
+            right = mid -1 
+        elif people < n:
+            left = mid +1
+    
+    return minutes
