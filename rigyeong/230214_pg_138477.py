6a5df3bbb8def7b5014a2090108c824d15bbb050
rigyeonghong <77164776+rigyeonghong@users.noreply.github.com>
2023-02-16 00:00:34 +0900

    ref: heapq

--- rigyeong/230214_pg_138477.py
+++ rigyeong/230214_pg_138477.py
@@ -7,3 +7,15 @@ def solution(k, score):
         if len(award) < k: answer.append(award[-1])
         else: answer.append(award[k-1])
     return answer
+
+# ref : 시간은 비슷
+import heapq
+def solution(k, score):
+    answer = []
+    award = []
+
+    for s in score:
+        heapq.heappush(award, -s)
+        answer.append(max(heapq.nsmallest(k, award))*(-1))
+
+    return answer
