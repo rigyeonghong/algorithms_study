9da8490feba16ecc02afe00963bbda0a984d840c
rigyeonghong <77164776+rigyeonghong@users.noreply.github.com>
2023-02-22 17:38:30 +0900

    ref:combinations, bisect

--- rigyeong/230222_pg_72412.py
+++ rigyeong/230222_pg_72412.py
@@ -1,48 +1,44 @@
-# 효율성 통과 못함
+from itertools import combinations
+from bisect import bisect_left
+
 def solution(info, query):
-    answer =[]
-    language={'cpp':[], 'java':[], 'python':[]}
-    job={'backend':[], 'frontend':[]}
-    career={'junior':[], 'senior':[]}
-    food ={'chicken':[], 'pizza':[]}
-    score =[]
-    p = [] 
-    
-    for idx, val in enumerate(info):
-        a,b,c,d,e = val.split()
-        language[a].append(idx)
-        job[b].append(idx)
-        career[c].append(idx)
-        food[d].append(idx)
-        score.append([int(e),idx])
-        p.append(idx) 
-        
-    score.sort()
-        
-    for q in query:
-        want = q.split(' ')
-        a,b,c,d,e = want[0], want[2], want[4], want[6], int(want[7])
-        flag_a, flag_b, flag_c,flag_d = False,False,False,False
-        
-        if a != '-': select_a = language[a]
-        else: select_a = p
-            
-        if b != '-': select_b = job[b]
-        else: select_b = p
-            
-        if c != '-': select_c = career[c]
-        else: select_c = p
-            
-        if d != '-': select_d = food[d]
-        else: select_d = p
-            
-        
-        select_e =[]
-        for s, people in score:
-            if s >= e:
-                select_e.append(people)
-                
-        result = list(set(select_a) & set(select_b) & set(select_c) & set(select_d) & set(select_e)) 
-        answer.append(len(result))
-    
+    answer = []
+    info_dict = {}
+
+    for i in info:
+        information = i.split()  # info안의 문자열을 공백을 기준으로 분리
+        mykey = information[:-1]  # info의 점수제외부분을 key로 분류
+        myval = information[-1]  # info의 점수부분을 value로 분류
+
+        for j in range(5):  # key들로 만들 수 있는 모든 조합 생성
+            for c in combinations(mykey, j):
+                tmp = ''.join(c)
+                if tmp in info_dict:
+                    info_dict[tmp].append(int(myval))  # 그 조합의 key값에 점수 추가
+                else:
+                    info_dict[tmp] = [int(myval)]
+
+    for k in info_dict:
+        info_dict[k].sort()  # dict안의 조합들을 점수순으로 정렬
+
+    for q in query:  # query도 마찬가지로 key와 value로 분리
+        myq = q.split(' ')
+        q_key = myq[:-1]
+        q_val = int(myq[-1])
+
+        while 'and' in q_key:  # and 제거
+            q_key.remove('and')
+        while '-' in q_key:  # - 제거
+            q_key.remove('-')
+        q_key = ''.join(q_key)  # dict의 key처럼 문자열로 변경
+
+        if q_key in info_dict:  # query의 key가 info_dict의 key로 존재하면
+            scores = info_dict[q_key]
+
+            if scores:  # score리스트에 값이 존재하면
+                enter = bisect_left(scores, q_val)
+                answer.append(len(scores) - enter)
+        else:
+            answer.append(0)
+
     return answer
