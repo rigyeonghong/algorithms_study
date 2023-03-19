a344e9ad0e74471d7a865408b265dd57bed7aaa5
rigyeonghong <zxcvbee@naver.com>
2023-03-19 17:29:26 +0900

    bs: 좀 더 직관적인 코드로 개선

--- rigyeong/230319_bj_2343.py
+++ rigyeong/230319_bj_2343.py
@@ -3,15 +3,11 @@ n,m = map(int, sys.stdin.readline().split())
 blueray = list(map(int, sys.stdin.readline().split()))
 
 ans = sum(blueray)
-start = ans//m
-end = 10000000
+start = max(blueray)
+end = ans 
 
 while start <= end:
     mid = (start + end) // 2
-    if mid < max(blueray): # 이게 포인트!! 밑에서 걸러주거나 해야함
-        start = mid + 1
-        continue
-
     cnt, len_blue = 1, 0
     for i in range(len(blueray)):
         add = len_blue + blueray[i]
